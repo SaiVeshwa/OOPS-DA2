5f06c5ff8e1b15e84a05fcf639ac9a1facd6b201
SaiVeshwa <131778103+SaiVeshwa@users.noreply.github.com>
2023-05-01 18:21:10 +0530

    Update program4.c

--- program4.c
+++ program4.c
@@ -1,65 +1,81 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-struct Employee {
+#define MAX_EMPLOYEES 100
+
+struct employee {
     char name[50];
     int age;
     char position[50];
     char date_of_joining[11];
 };
 
-
-int compareNames(const void* a, const void* b) {
-    struct Employee* emp1 = (struct Employee*) a;
-    struct Employee* emp2 = (struct Employee*) b;
-    return strcmp(emp1->name, emp2->name);
+void sort_by_name(struct employee emp[], int n) {
+    struct employee temp;
+    int i, j;
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(strcmp(emp[i].name, emp[j].name) > 0) {
+                temp = emp[i];
+                emp[i] = emp[j];
+                emp[j] = temp;
+            }
+        }
+    }
 }
 
+void sort_by_date_of_joining(struct employee emp[], int n) {
+    struct employee temp;
+    int i, j;
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(strcmp(emp[i].date_of_joining, emp[j].date_of_joining) > 0) {
+                temp = emp[i];
+                emp[i] = emp[j];
+                emp[j] = temp;
+            }
+        }
+    }
+}
 
-int compareJoiningDates(const void* a, const void* b) {
-    struct Employee* emp1 = (struct Employee*) a;
-    struct Employee* emp2 = (struct Employee*) b;
-    return strcmp(emp1->date_of_joining, emp2->date_of_joining);
+void display_employee_list(struct employee emp[], int n) {
+    int i;
+    for(i=0; i<n; i++) {
+        printf("Name: %s\n", emp[i].name);
+        printf("Age: %d\n", emp[i].age);
+        printf("Position: %s\n", emp[i].position);
+        printf("Date of Joining: %s\n\n", emp[i].date_of_joining);
+    }
 }
 
 int main() {
     int n, i;
+    struct employee emp[MAX_EMPLOYEES];
+
     printf("Enter the number of employees: ");
     scanf("%d", &n);
-    struct Employee employees[n];
-    
-   
-    for (i = 0; i < n; i++) {
-        printf("Enter details of employee %d:\n", i + 1);
+
+    for(i=0; i<n; i++) {
+        printf("Enter details of employee %d:\n", i+1);
         printf("Name: ");
-        scanf("%s", employees[i].name);
+        scanf("%s", emp[i].name);
         printf("Age: ");
-        scanf("%d", &employees[i].age);
+        scanf("%d", &emp[i].age);
         printf("Position: ");
-        scanf("%s", employees[i].position);
+        scanf("%s", emp[i].position);
         printf("Date of joining (dd/mm/yyyy): ");
-        scanf("%s", employees[i].date_of_joining);
+        scanf("%s", emp[i].date_of_joining);
     }
-    
-    
-    qsort(employees, n, sizeof(struct Employee), compareNames);
-    printf("\nEmployee List sorted by name:\n");
-    for (i = 0; i < n; i++) {
-        printf("Name: %s\n", employees[i].name);
-        printf("Age: %d\n", employees[i].age);
-        printf("Position: %s\n", employees[i].position);
-        printf("Date of Joining: %s\n\n", employees[i].date_of_joining);
-    }
-    
-    qsort(employees, n, sizeof(struct Employee), compareJoiningDates);
-    printf("Employee List sorted by date of joining:\n");
-    for (i = 0; i < n; i++) {
-        printf("Name: %s\n", employees[i].name);
-        printf("Age: %d\n", employees[i].age);
-        printf("Position: %s\n", employees[i].position);
-        printf("Date of Joining: %s\n\n", employees[i].date_of_joining);
-    }
-    
+
+    sort_by_name(emp, n);
+
+    printf("\nEmployee List sorted by name:\n\n");
+    display_employee_list(emp, n);
+
+    sort_by_date_of_joining(emp, n);
+
+    printf("\nEmployee List sorted by date of joining:\n\n");
+    display_employee_list(emp, n);
+
     return 0;
 }
